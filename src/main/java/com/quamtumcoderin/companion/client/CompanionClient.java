c48d0884241829565cc40c6c10a358ba9a312e0c
quamtumcoderin <killerestrada8@gmail.com>
2026-02-15 17:26:33 +0100

    Fase 2: Renderizado (Hacerlo Visible) En lugar de crear un modelo desde cero, reutilizaremos el modelo BipedEntityModel (el modelo de Steve/Alex para que el compañero pueda llevar armadura y herramientas visualmente.

--- src/main/java/com/quamtumcoderin/companion/client/CompanionClient.java
+++ src/main/java/com/quamtumcoderin/companion/client/CompanionClient.java
@@ -1,4 +1,19 @@
 package com.quamtumcoderin.companion.client;
 
-public class CompanionClient {
+import com.quamtumcoderin.companion.client.renderer.CompanionRenderer;
+import com.quamtumcoderin.companion.registry.ModEntities;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+
+@Environment(EnvType.CLIENT)
+public class CompanionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        EntityRendererRegistry.INSTANCE.register(
+                ModEntities.COMPANION,
+                (dispatcher, context) -> new CompanionRenderer(dispatcher)
+        );
+    }
 }
--- src/main/java/com/quamtumcoderin/companion/client/renderer/CompanionRenderer.java
+++ src/main/java/com/quamtumcoderin/companion/client/renderer/CompanionRenderer.java
@@ -1,4 +1,22 @@
 package com.quamtumcoderin.companion.client.renderer;
 
-public class CompanionRenderer {
+import com.quamtumcoderin.companion.entity.CompanionEntity;
+import net.minecraft.client.render.entity.BipedEntityRenderer;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.util.Identifier;
+
+public class CompanionRenderer extends BipedEntityRenderer<CompanionEntity, BipedEntityModel<CompanionEntity>> {
+
+    private static final Identifier TEXTURE = new Identifier("companionmod", "textures/entity/companion_texture.png");
+
+    public CompanionRenderer(EntityRenderDispatcher dispatcher) {
+        super(dispatcher, new PlayerEntityModel<>(0.0F, false), 0.5F);
+    }
+
+    @Override
+    public Identifier getTexture(CompanionEntity entity) {
+        return TEXTURE;
+    }
 }
